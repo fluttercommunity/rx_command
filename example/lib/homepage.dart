5ce8c0577e8b8fd5e24619b77f29c6778e3c7e55
escamoteur <github@burkharts.net>
2018-09-05 11:46:23 +0200

    updated example

--- example/lib/homepage.dart
+++ example/lib/homepage.dart
@@ -3,114 +3,107 @@ import 'package:flutter/material.dart';
 import 'listview.dart';
 import 'main.dart';
 
- 
- class HomePage extends StatelessWidget
- {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return 
-         new Scaffold(
-            appBar: new AppBar(title: new Text("WeatherDemo")),
-            body: 
-              new Column(children: <Widget>
-              [
-               new Padding(padding: const EdgeInsets.all(5.0),child: 
-                      new TextField(
-                              autocorrect: false,
-                              decoration: new InputDecoration(
-                                                  hintText: "Filter cities",
-                                                  hintStyle: new TextStyle(color: new Color.fromARGB(150, 0, 0, 0)),
-                                                  ),
-                              style: new TextStyle(
-                                            fontSize: 20.0,
-                                            color: new Color.fromARGB(255, 0, 0, 0)),
-                              onChanged: TheViewModel.of(context).textChangedCommand,),
-                ),
-
-                new Expanded( child: 
-                      // Handle events to show / hide spinner
-                      new StreamBuilder<bool>(   
-                          stream: TheViewModel.of(context).updateWeatherCommand.isExecuting, 
-                          builder: (BuildContext context, AsyncSnapshot<bool> isRunning)  
-                              {
-                                 // if true we show a buys Spinner otherwise the ListView
-                                if (isRunning.hasData && isRunning.data == true)
-                                {
-                                    return new Center(child: new Container(width: 50.0, height:50.0, child: new CircularProgressIndicator())); 
-                                }
-                                else
-                                {
-                                   return new WeatherListView();                                  }
-                            })                                              
-                          ),
-                
-                
-                new Padding(padding: const EdgeInsets.all(8.0),
-                    child: 
-                        // We use a stream builder to toggle the enabled state of the button
-                      new Row(
-                        children: <Widget>[
-                          new Expanded(
-                                child: new StreamBuilder<bool>(   // Streambuilder rebuilds its subtree on every item the stream issues
-                                stream: TheViewModel.of(context).updateWeatherCommand.canExecute,   //We access our ViewModel through the inherited Widget
-                                builder: (BuildContext context, AsyncSnapshot<bool> snapshot)  
-                                    {
-                                      VoidCallback handler;
-                                      if (snapshot.hasData)
-                                      {
-                                          // Depending on teh Value we get from the stream we set or clear the Handler
-                                          handler = snapshot.data ? TheViewModel.of(context).updateWeatherCommand :null; 
-                                      }
-                                      return new RaisedButton(                               
-                                              child: 
-                                                  new Text("Update"), 
-                                              color: new Color.fromARGB(255, 33, 150, 243),
-                                              textColor: new Color.fromARGB(255, 255, 255, 255),
-                                              onPressed: handler,
-                                              );
-                                      
-                                  }),
-                          ),
-                                new StateFullSwitch(state: true,
-                                    onChanged: TheViewModel.of(context).switchChangedCommand)
-                        ],
-                      )                                              
-                
-                ),
-                
-              ],
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("WeatherDemo")),
+      body: new Column(
+        children: <Widget>[
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: new TextField(
+              autocorrect: false,
+              decoration: new InputDecoration(
+                hintText: "Filter cities",
+                hintStyle: new TextStyle(color: new Color.fromARGB(150, 0, 0, 0)),
+              ),
+              style: new TextStyle(fontSize: 20.0, color: new Color.fromARGB(255, 0, 0, 0)),
+              onChanged: TheViewModel.of(context).textChangedCommand,
             ),
-          );
+          ),
+          new Expanded(
+              child:
+                  // Handle events to show / hide spinner
+                  new StreamBuilder<bool>(
+                      stream: TheViewModel.of(context).updateWeatherCommand.isExecuting,
+                      builder: (BuildContext context, AsyncSnapshot<bool> isRunning) {
+                        // if true we show a buys Spinner otherwise the ListView
+                        if (isRunning.hasData && isRunning.data == true) {
+                          return new Center(
+                              child: new Container(
+                                  width: 50.0,
+                                  height: 50.0,
+                                  child: new CircularProgressIndicator()));
+                        } else {
+                          return new WeatherListView();
+                        }
+                      })),
+          new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  // We use a stream builder to toggle the enabled state of the button
+                  new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new StreamBuilder<bool>(
+                        // Streambuilder rebuilds its subtree on every item the stream issues
+                        stream: TheViewModel.of(context)
+                            .updateWeatherCommand
+                            .canExecute, //We access our ViewModel through the inherited Widget
+                        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                          VoidCallback handler;
+                          if (snapshot.hasData) {
+                            // Depending on teh Value we get from the stream we set or clear the Handler
+                            handler = snapshot.data
+                                ? TheViewModel.of(context).updateWeatherCommand
+                                : null;
+                          }
+                          return new RaisedButton(
+                            child: new Text("Update"),
+                            color: new Color.fromARGB(255, 33, 150, 243),
+                            textColor: new Color.fromARGB(255, 255, 255, 255),
+                            onPressed: handler,
+                          );
+                        }),
+                  ),
+                  new StateFullSwitch(
+                      state: true, onChanged: TheViewModel.of(context).switchChangedCommand)
+                ],
+              )),
+        ],
+      ),
+    );
   }
-   
- }
- 
- /// As the normal switch does not even remeber and display its current state 
- ///  we us this one 
-class StateFullSwitch extends StatefulWidget
-{
-    final bool state;
-    final ValueChanged<bool> onChanged;
-
-    StateFullSwitch({this.state, this.onChanged});
-
-    @override
-    StateFullSwitchState createState() {
-    return new StateFullSwitchState(state, onChanged);
-    }
-
 }
 
-class StateFullSwitchState extends State<StateFullSwitch> 
-{
-    
-    bool state;
-    ValueChanged<bool> handler;
-    
-    StateFullSwitchState(this.state, this.handler);    
+/// As the normal switch does not even remeber and display its current state
+///  we us this one
+class StateFullSwitch extends StatefulWidget {
+  final bool state;
+  final ValueChanged<bool> onChanged;
 
-    @override 
-    Widget build(BuildContext context) {
-        return new Switch(value: state, onChanged: (b) { setState(()=> state = b); handler(b);});
-        }
+  StateFullSwitch({this.state, this.onChanged});
+
+  @override
+  StateFullSwitchState createState() {
+    return new StateFullSwitchState(state, onChanged);
+  }
+}
+
+class StateFullSwitchState extends State<StateFullSwitch> {
+  bool state;
+  ValueChanged<bool> handler;
+
+  StateFullSwitchState(this.state, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Switch(
+        value: state,
+        onChanged: (b) {
+          setState(() => state = b);
+          handler(b);
+        });
+  }
 }
--- example/lib/weather_viewmodel.dart
+++ example/lib/weather_viewmodel.dart
@@ -7,86 +7,78 @@ import 'package:http/http.dart' as http;
 
 import 'json/weather_in_cities.dart';
 
+class WeatherViewModel {
+  RxCommand<String, List<WeatherEntry>> updateWeatherCommand;
+  RxCommand<bool, bool> switchChangedCommand;
+  RxCommand<String, String> textChangedCommand;
 
-  class WeatherViewModel {
+  WeatherViewModel() {
+    // Command expects a bool value when executed and issues the value on it's result Observable (stream)
+    switchChangedCommand = RxCommand.createSync3<bool, bool>((b) => b);
 
-    RxCommand<String,List<WeatherEntry>>  updateWeatherCommand;
-    RxCommand<bool,bool>  switchChangedCommand;
-    RxCommand<String,String>  textChangedCommand;
+    // We pass the result of switchChangedCommand as canExecute Observable to the upDateWeatherCommand
+    updateWeatherCommand = RxCommand.createAsync3<String, List<WeatherEntry>>(update,
+        canExecute: switchChangedCommand, emitsLastValueToNewSubscriptions: true);
 
+    // Will be called on every change of the searchfield
+    textChangedCommand = RxCommand.createSync3((s) => s);
 
-    WeatherViewModel()
-    {
+    // handler for results
+    textChangedCommand
+        .debounce(new Duration(
+            milliseconds:
+                500)) // make sure we start processing only if the user make a short pause typing
+        .listen((filterText) {
+      updateWeatherCommand.execute(
+          filterText); // I could omit he execute because RxCommand is a callable class but here it
+      //  makes the intention clearer
+    });
 
-        // Command expects a bool value when executed and issues the value on it's result Observable (stream)
-        switchChangedCommand = RxCommand.createSync3<bool,bool>((b)=>b);
+    updateWeatherCommand.thrownExceptions.listen((ex) => print(ex.toString()));
 
-        // We pass the result of switchChangedCommand as canExecute Observable to the upDateWeatherCommand
-        updateWeatherCommand = RxCommand.createAsync3<String,List<WeatherEntry>>(update, canExecute:  switchChangedCommand.results);
+    // Update data on startup
+    updateWeatherCommand.execute();
+  }
 
-        // Will be called on every change of the searchfield
-        textChangedCommand = RxCommand.createSync3((s) => s);
+  // Async function that queries the REST API and converts the result into the form our ListViewBuilder can consume
+  Future<List<WeatherEntry>> update(String filtertext) {
+    const url =
+        "http://api.openweathermap.org/data/2.5/box/city?bbox=5,47,14,54,20&appid=27ac337102cc4931c24ba0b50aca6bbd";
 
-        // handler for results
-        textChangedCommand.results
-          .debounce( new Duration(milliseconds: 500))  // make sure we start processing only if the user make a short pause typing 
-            .listen( (filterText)
+    var httpStream = new Observable(http.get(url).asStream());
+
+    return httpStream.where((data) => data.statusCode == 200) // only continue if valid response
+        .map((data) // convert JSON result into a List of WeatherEntries
             {
-              updateWeatherCommand.execute( filterText); // I could omit he execute because RxCommand is a callable class but here it 
-                                                         //  makes the intention clearer
-            });  
-
-        // Update data on startup
-        updateWeatherCommand.execute();
-    }
-
-
-
-    // Async function that queries the REST API and converts the result into the form our ListViewBuilder can consume
-    Future<List<WeatherEntry>> update(String filtertext)
-    {
-
-
-      const url = "http://api.openweathermap.org/data/2.5/box/city?bbox=5,47,14,54,20&appid=27ac337102cc4931c24ba0b50aca6bbd";  
-      
-
-      var httpStream = new Observable(http.get(url).asStream()); 
-
-      return httpStream
-              .where((data) => data.statusCode == 200)  // only continue if valid response
-                .map( (data) // convert JSON result into a List of WeatherEntries
-                {
-                      return new WeatherInCities.fromJson(json.decode(data.body) as Map<String,dynamic> ).Cities // we are only interested in the Cities part of the response
-                        .where( (weatherInCity) =>  filtertext ==null || 
-                                                    filtertext.isEmpty ||  // if filtertext is null or empty we return all returned entries
-                                                    weatherInCity.Name.toUpperCase().startsWith(filtertext.toUpperCase())) // otherwise only matching entries
-                          .map((weatherInCity) => new WeatherEntry(weatherInCity) ) // Convert City object to WeatherEntry
-                            .toList(); // aggregate entries to a List
-                            
-                }).first; // Return result as Future
-          
-    }
- 
-    }
-                          
- 
+      return new WeatherInCities.fromJson(json.decode(data.body) as Map<String, dynamic>)
+          .Cities // we are only interested in the Cities part of the response
+          .where((weatherInCity) =>
+              filtertext == null ||
+              filtertext.isEmpty || // if filtertext is null or empty we return all returned entries
+              weatherInCity.Name.toUpperCase()
+                  .startsWith(filtertext.toUpperCase())) // otherwise only matching entries
+          .map((weatherInCity) =>
+              new WeatherEntry(weatherInCity)) // Convert City object to WeatherEntry
+          .toList(); // aggregate entries to a List
+    }).first; // Return result as Future
+  }
+}
 
 class WeatherEntry {
-   String cityName;
-   String iconURL;
-   double wind;
-   double rain;
-   double temperature;
-   String description;
+  String cityName;
+  String iconURL;
+  double wind;
+  double rain;
+  double temperature;
+  String description;
 
-  WeatherEntry(City city)
-  {
-      this.cityName = city.Name;
-      this.iconURL = city.weather != null ?  "http://openweathermap.org/img/w/${city.weather[0].Icon}.png" :  null;
-      this.description = city.weather != null ?  city.weather[0].Description : null;
-      this.wind =city.wind.Speed;
-      this.rain = rain;
-      this.temperature = city.main.Temp;
-
+  WeatherEntry(City city) {
+    this.cityName = city.Name;
+    this.iconURL =
+        city.weather != null ? "http://openweathermap.org/img/w/${city.weather[0].Icon}.png" : null;
+    this.description = city.weather != null ? city.weather[0].Description : null;
+    this.wind = city.wind.Speed;
+    this.rain = rain;
+    this.temperature = city.main.Temp;
   }
 }
